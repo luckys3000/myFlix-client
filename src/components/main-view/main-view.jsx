47d171896ee3965799f6280ef9fc38cef378f3fb
luckys3000 <jwillard@396willard.com>
2024-10-03 16:59:42 -0500

    build: create login view and signup view
     - Create login-view and signup-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,14 +1,25 @@
 import { useState, useEffect } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { SignupView } from ' ../signup-view/signup-view';
 
 export const MainView = () => {
+	const storedUser = JSON.parse(localStorage.getItem('user'));
+	const storedToken = localStorage.getItem('token');
+	const [user, setUser] = userState(storedUser ? storedUser : null);
+	const [token, setToken] = userState(storedToken ? storedToken : null);
 	const [movies, setMovies] = useState([]);
-
 	const [selectedMovie, setSelectedMovie] = useState(null);
 
 	useEffect(() => {
-		fetch('https://luckyflix3000-b3f882eb1652.herokuapp.com/movies')
+		if (!token) {
+			return;
+		}
+
+		fetch('https://luckyflix3000-b3f882eb1652.herokuapp.com/movies', {
+			headers: { Authorization: `Bearer ${token}` },
+		})
 			.then((response) => response.json())
 			.then((data) => {
 				const moviesFromApi = data.map((movie) => {
@@ -25,7 +36,22 @@ export const MainView = () => {
 
 				setMovies(moviesFromApi);
 			});
-	}, []);
+	}, [token]);
+
+	if (!user) {
+		return (
+			<>
+				<LoginView
+					onLoggedIn={(user, token) => {
+						setUser(user);
+						setToken(token);
+					}}
+				/>
+				or
+				<SignupView />
+			</>
+		);
+	}
 
 	if (selectedMovie) {
 		return <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />;
@@ -46,6 +72,14 @@ export const MainView = () => {
 					}}
 				/>
 			))}
+			<button
+				onClick={() => {
+					setUser(null);
+					setToken(null);
+					localStorage.clear();
+				}}>
+				Logout
+			</button>
 		</div>
 	);
 };
